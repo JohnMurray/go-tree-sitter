23c3a418636d77350e046e4743a8aecbbc80dc33
John Murray <me@johnmurray.io>
2024-10-01 13:10:59 +0000

    add cgo flags

--- include/tree_sitter/stub.go
+++ include/tree_sitter/stub.go
@@ -1,5 +1,8 @@
 package tree_sitter
 
+/*
+#cgo CFLAGS: -std=c11 -D_POSIX_C_SOURCE=200112L
+*/
 import "C"
 
 func init() {}
--- src/unicode/stub.go
+++ src/unicode/stub.go
@@ -1,5 +1,8 @@
 package unicode
 
+/*
+#cgo CFLAGS: -I../../include -std=c11 -D_POSIX_C_SOURCE=200112L
+*/
 import "C"
 
 func init() {}
